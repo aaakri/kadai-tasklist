d23bb7df580261c636436e454f83accd7bd36b51
akari.kumagai <ubuntu@ip-172-31-18-109.ap-northeast-1.compute.internal>
2021-04-27 12:20:17 +0000

    [Add]モデル完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  root to: "tasks#index"
-  resources:tasks
+ get "signup", to: "users#new"
+  resources:tasks, only: [:index, :show, :create]
   
 end
